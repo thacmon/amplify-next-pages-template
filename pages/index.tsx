fb2abb913eb916c3140d4d38d1957de11ec7aabc
“Tsachi <thacmon@amazon.ch>
2024-07-08 16:12:04 +0200

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,6 +35,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
     <main>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
